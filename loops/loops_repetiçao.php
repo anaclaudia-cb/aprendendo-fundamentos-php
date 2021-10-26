39773d39ac8dcbe0d99a5e0f6857d97b59c54a57
Ana Claudia <anaclaudia@yahp.com.br>
2021-10-26 17:25:35 -0300

    loop FOR

---
+++ loops/loops_repetiçao.php
@@ -0,0 +1,24 @@
+<?php
+
+// FOR
+
+$name = ['a', 'b', 'c', 'd', 'e', 'f'];
+
+echo '<hr>';
+
+for ($i = 0; $i<count($name); $i++) {
+    echo $name[$i]; 
+}
+
+//loop infinito com ++ por isso troca por --
+// for ($i = 10; $i>0; $i--) {
+//     echo "{$i} <br>"; 
+// }
+
+//for usa a variavel $i
+// for ($i = 0; $i<10; $i++) {
+//     echo "{$i} <br>"; 
+// }
+
+
+echo '<hr>';
