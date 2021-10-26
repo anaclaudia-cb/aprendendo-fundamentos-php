741ef48985fb02faddb3677eaa6422a2cb635bc6
Ana Claudia <anaclaudia@yahp.com.br>
2021-10-26 17:46:30 -0300

    while

---
+++ loops/while.php
@@ -0,0 +1,22 @@
+<?php
+
+$names = ['a', 'b', 'c'];
+
+//verifica depois entra no loop
+$i = 0;
+
+while ($i < count($names)){
+    echo $names[$i];
+
+    $i++;
+}
+
+echo '<hr>';
+
+//entra no loop depois verifica
+$a = 1;
+
+do{
+    echo $a;
+    $a++;
+} while ($a <= 10);
