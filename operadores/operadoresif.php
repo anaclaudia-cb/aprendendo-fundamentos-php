26cf0f68fce94e710f9cf58fa29b379190802c90
Ana Claudia <anaclaudia@yahp.com.br>
2021-10-26 16:17:00 -0300

    if

---
+++ operadores/operadoresif.php
@@ -0,0 +1,15 @@
+<?php
+
+$number = 12;
+$numberTwo = 12;
+
+// se
+if ($number == $numberTwo) {
+    echo 'É igual';
+}
+
+// outra forma de apresentar o if
+
+if ($number == $numberTwo && $number ==12): 
+    echo 'É igual';
+endif;
