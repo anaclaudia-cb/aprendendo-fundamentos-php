59083378a5e86fa852065e32143e6e428fc5129d
Ana Claudia <anaclaudia@yahp.com.br>
2021-11-01 20:34:37 -0300

    ltrim, rtrim, trim

---
+++ strings/strings3.php
@@ -0,0 +1,11 @@
+<?php
+
+$name = ' EspecializaTi ';
+
+var_dump($name);
+//       tira o espaço da string no começo
+var_dump(ltrim($name));
+//       tira o espaço da string no final
+var_dump(rtrim($name));
+//       tira o espaço do inicio e do final
+var_dump(trim($name));
