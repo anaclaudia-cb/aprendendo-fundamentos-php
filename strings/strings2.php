0bd7422ec67f3f7e4fa601beccc784d19aae6c39
Ana Claudia <anaclaudia@yahp.com.br>
2021-11-01 20:30:17 -0300

    explode, implode

---
+++ strings/strings2.php
@@ -0,0 +1,16 @@
+<?php
+
+// $state = 'São Paulo/SP';
+// $arrayState = explode('/', $state);
+// var_dump($state);
+
+$info = 'São Paulo/SP/Brasil/Terra';
+$arrayState = explode('/', $info);
+var_dump($arrayState);
+
+echo '<hr>';
+
+// echo implode($arrayState, '#');
+
+$arrayTeste = [1,2,3,4,5];
+//  echo implode($arrayTeste, ' - ');
