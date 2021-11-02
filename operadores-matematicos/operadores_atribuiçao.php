9da273e70e9b779503cd950be8d4b04bfb9c7320
Ana Claudia <anaclaudia@yahp.com.br>
2021-11-02 14:38:52 -0300

    = += -= *= /=

---
+++ operadores-matematicos/operadores_atribuiçao.php
@@ -0,0 +1,15 @@
+<?php
+
+// operador de atribuição (=)
+$a = 12;
+echo "{$a} <br>";
+$a += 14; #$a = $a + 12;
+echo "{$a} <br>";
+$a -= 14; #$a = $a - 14;
+echo "{$a} <br>";
+$a *= 2; #$a = $a * 14;
+echo "{$a} <br>";
+$a /= 2; #$a = $a / 14;
+echo "{$a} <br>";
+
+
